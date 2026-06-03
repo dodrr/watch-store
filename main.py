d12a044ed7d89a8dd036ad28fa63727ceb2169f4
Hayatuna <bahytbekoverasyl@gmail.com>
2026-06-03 11:39:49 +0500

    Update CRUD

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-app = FastAPI() 
+app = FastAPI()
 orders = []
 products = [
-    {"brand": "Casio", "price": 10000},
-    {"brand": "Hyblot", "price": 145444},
-    {"brand": "Patek philippe", "price": 45555}
+    {"brand": "Casio", "price": 50000},
+    {"brand": "Rolex", "price": 67000},
+    {"brand": "Hublot", "price": 35000}
 ]
 
 class Order(BaseModel):
@@ -18,42 +18,35 @@ def get_product():
     return products
 
 @app.get("/products")
-def search_order(max_price: int = None):
-
-    if max_price:
+def search_products(max_price: int = None):
+    if id >= len(products):
         return [p for p in products if p["price"] <= max_price]
-    else:
-        return products
+    return products
 
 @app.post("/order")
-def creat_order(order: Order):
+def create_order(order: Order):
     orders.append(order)
     return {
-        "message": "Заказ принят",
-        "watch": order.watch,
-        "customer": order.customer
+        "message": "Закза принят"
     }
-
-@app.get("/orders")
+@app.get("/order")
 def get_orders():
     return orders
 
 @app.put("/orders/{id}")
-def update_order(id: int, order: Order):
+def update_orders(id: int, order: Order):
     if id >= len(orders):
-        raise HTTPException(status_code=404, detail="Заказ не")
+        raise HTTPException(status_code=404, detail="Заказ не найден")
     orders[id] = order
     return {
         "message": "Заказ обновлен"
     }
 
 @app.delete("/orders/{id}")
-def delete_order(id: int):
-    if id >= len(orders):
+def delete_orders(id: int):
+    if id >= len(products):
         raise HTTPException(status_code=404, detail="Заказ не найден")
     orders.pop(id)
     return {
         "message": "Заказ удален"
     }
-
-
