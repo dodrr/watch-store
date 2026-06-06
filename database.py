7aaa5e5146a1f6ca6f4b0d0d682cd25b59c5d7a2
Hayatuna <bahytbekoverasyl@gmail.com>
2026-06-06 15:59:12 +0500

    Add models

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = "postgresql://postgres:erasyl2809@localhost/watch-store"
 
-engine = DATABASE_URL
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
